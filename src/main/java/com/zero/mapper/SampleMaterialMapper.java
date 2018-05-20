eba7efda2178826b7dabcb4337fcfe2f3f3c37a6
za-wangshenhua <za-wangshenhua@zaonline.com>
2018-05-20 21:02:49 +0800

    modify

--- src/main/java/com/zero/mapper/SampleMaterialMapper.java
+++ src/main/java/com/zero/mapper/SampleMaterialMapper.java
@@ -3,6 +3,8 @@ package com.zero.mapper;
 import com.zero.model.SampleMaterial;
 
 import java.util.List;
+
+import com.zero.model.example.SampleMaterialExample;
 import org.apache.ibatis.annotations.Param;
 
 public interface SampleMaterialMapper {
